6aaf47dd79f08a0500cd975e89cdc687748d6a03
prarthanagalani <prarthanagalani@gmail.com>
2023-11-22 18:52:52 +0530

    added: welcome message_day2

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package LineComparison_day2;
+
+class LineComparison {
+
+    public static void main(String[] args) {
+
+        // welcome message on main branch
+        System.out.println("Welcome to Line Comparison Computation Program!!");
+
+    }
+}
